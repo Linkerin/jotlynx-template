13e9d2e8b68cd72e17c570feb6bdd0f01dffa441
Linkerin <130695447+Linkerin@users.noreply.github.com>
2025-08-20 23:57:37 +0300

    feat: implemented color scheme switch. Added new config file. Improved layout both for desktop and mobile

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+const config = {
+  author: '',
+  siteUrl: '',
+  title: 'JotLynx website',
+  description:
+    'JotLynx is a static site generator from Jolin markdown notes based on Astro.',
+  keywords: '',
+  ogImage: '',
+  socials: {
+    github: '',
+    twitter: '',
+    linkedin: '',
+    bluesky: '',
+    email: ''
+  },
+  htmlLang: 'en',
+  hasAbout: false
+} as const;
+
+export default config;
