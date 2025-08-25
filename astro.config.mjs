3587a171c4d00df4b16e31e0d3f4286c18a662b5
Linkerin <130695447+Linkerin@users.noreply.github.com>
2025-08-25 14:56:45 +0300

    feat: added sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({
+import websiteConfig from './src/website.config.json';
+import deepMerge from './src/utils/deepMerge';
+
+const config = {
   markdown: {
     shikiConfig: {
       themes: {
@@ -18,4 +18,25 @@ export default defineConfig({
   },
   integrations: [mdx(), sitemap()],
   prefetch: true
-});
+};
+
+// https://astro.build/config
+export default defineConfig(
+  deepMerge(
+    {
+      // @ts-ignore
+      markdown: {
+        shikiConfig: {
+          themes: {
+            light: 'github-light',
+            dark: 'github-dark-dimmed'
+          },
+          defaultColor: 'light-dark()'
+        }
+      },
+      integrations: [mdx(), sitemap()],
+      prefetch: true
+    },
+    websiteConfig.astro ?? {}
+  )
+);
